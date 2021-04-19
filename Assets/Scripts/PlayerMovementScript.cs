d2dfa9d4822b3f23f42c6d3a7d603608b7e8a4ea
A. Toprak Firat <atoprakfirat@gmail.com>
2021-04-19 22:03:55 +0300

    New Movment

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -8,37 +8,68 @@ public class PlayerMovementScript : MonoBehaviour
     public Vector2 direction;
     float rotationZ;
 
+    public float timer = 0f;
+    public float velocity = 0f;
+    public float acceler = 0.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
+        float maxspeed = 20f;
 
-        float speed = 10f;
+
 
         if (Input.GetKey("w"))
         {
-            pos.y += speed * Time.deltaTime;
+            pos.y += velocity * Time.deltaTime * 1.2f;
         }
+
         if (Input.GetKey("s"))
         {
-            pos.y -= speed * Time.deltaTime;
+            pos.y -= velocity * Time.deltaTime;
         }
         if (Input.GetKey("d"))
         {
-            pos.x += speed * Time.deltaTime;
+            pos.x += velocity * Time.deltaTime;
+
         }
         if (Input.GetKey("a"))
         {
-            pos.x -= speed * Time.deltaTime;
+            pos.x -= velocity * Time.deltaTime;
+        }
+
+        //EGER HIC BISIYE BASMIYORSA TIMER'I SIFIRLA
+        //YAVAS YAVAS VELOCITY 0'A DUSSUN DIYE HER INSTANCE'TA CIKAR, 0'A GELINCE DUR
+
+        if (!Input.GetKey("w") && !Input.GetKey("s") && !Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            timer = 0f;
+            velocity = Mathf.Max(0f, velocity - 0.5f);
+        }
+
+        //TIMER BASIS SURESINI ALGILIYOR HER INSTANCE'TA
+        //EGER BI TUSA BASILIRSA VELOCITY'YE ACCELERATION EKLE MAXSPEEDE ULASANA KADAR
+
+        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d"))
+        {
+            timer += Time.deltaTime;
+            velocity += timer * 10 * acceler;
+            velocity = Mathf.Min(velocity, maxspeed);
+            
+
         }
 
         transform.position = pos;
+
+        
 
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -46,6 +77,8 @@ public class PlayerMovementScript : MonoBehaviour
         rotationZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
         direction.Normalize();
+
+        Debug.Log(direction);
 
         if (Input.GetMouseButtonDown(0))
         {
