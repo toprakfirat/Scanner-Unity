cbc1686cb18ade2cbcd406fd7df831474f3f4c6e
egecark <ecarkoglu14@ku.edu.tr>
2021-04-19 17:13:54 -0400

    amk toprak

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -118,7 +118,7 @@ public class PlayerMovementScript : MonoBehaviour
 
     public void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) 
         {
             if (collision.gameObject.tag == "Object")
             {
